9a00b846e5b9b45e79a465e12f53cbbc310a8814
Gnorr <gnorrbreed@gmail.com>
2020-06-22 16:10:05 +0300

    dz

---
+++ functions/function.js
@@ -0,0 +1,46 @@
+
+//задача 1
+
+
+// let x =prompt('first numder');
+// let y = prompt ('second number');
+// let z = prompt ('third number')
+
+
+
+
+// function minNum(x, y, z) {
+// 	let min = x;
+// 	if ( min < y && min < z){
+// 	}else if (y < z){
+// 		min =y ;
+// 	}else{
+// 		min = z;
+// 	}
+
+
+// 	return min;
+
+
+// }
+
+// alert(minNum(x, y, z));
+
+
+
+
+//задача 3
+
+// let x = prompt('data');
+
+// function typeName(x) {
+// 	typeof x;
+
+// 	return typeof x;
+
+
+// }
+
+
+// console.log(typeName());
+
