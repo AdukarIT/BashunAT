ba49894babbca477f58511f4be5ebf832631baa1
Gnorr <gnorrbreed@gmail.com>
2020-07-07 19:35:18 +0300

    dz

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -1,29 +1,76 @@
 function getRandomArray(len = 5) {
 	let arr = [];
+
 		for( let i = 0; i < len; i++){
-			arr[i] = Math.random();
+			arr[i] = Math.floor(Math.random() * 10);
 		}
 		return arr;
 }		
 
 console.log (getRandomArray());
 
+//задача 2
+
+// function arrSum (arr){
+// 	let sum = 0;
+
+// 	for (let i = 0; i < arr.length; i++) {
+// 		sum += arr[i];
+// 	}
+// 	let dev = sum / arr.length;
+
+// 	for (let i = 0; i < arr.length; i++) {
+// 		if (dev < arr[i]){
+			
+// 		}
+// 		return(arr[i]);
+// 	}
+
+// }
+
+// console.log (arrSum(getRandomArray()));
 
 
-function arrSum (array){
-	let sum = 0;
-	for (let i = 0; i < array.length; i++) {
-		sum += array[i];
-	}
-	let dev = sum / array.length;
-	for (let i = 0; i < array.length; i++) {
-		if (div < array[i]){
-			console.log(array[i]);
-		}
-	}
+// //задача 3
 
-}
+ /*сортирует но слайс не прикрутил. извините что так мало успел. я все дни работал. 
+ да и сортировка берет откуда то indifinde */
 
-console.log (arrSum(getRandomArray));
+// function sortNumber(arr) {
+// 	let = arr;
+// 	arr.sort();
+	
+// }
+
+
+
+// console.log (sortNumber(getRandomArray()));
+
+
+
+// function minNumber(arr) {
+	
+// 	let  smallNum = Infinity;
+// 	let  smallerNum = Infinity;
+
+// 	for (var i = 0; i < arr.length; i++) {
+// 		if (arr[i] < smallNum) {
+// 			smallNum = arr[i];
+// 		}
+
+// 	}
+
+// 	for (var i = 0; i < arr.length; i++) {
+// 		if (arr[i] < smallerNum && arr[i] > smallNum) {
+// 			smallerNum = arr[i];
+// 		}
+// 	}
+// 	return[smallerNum, smallNum];
+
+// }
+
+// console.log (minNumber(getRandomArray()));
+
+
 
 
